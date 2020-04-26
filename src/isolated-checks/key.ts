752a38ae2f7f473b8004cbea1b77c02d4049be28
sepehr <sepehralizade@live.com>
2020-04-26 08:17:02 +0300

    beta one key fix

--- src/isolated-checks/key.ts
+++ src/isolated-checks/key.ts
@@ -1,3 +1,34 @@
-// keys don't require extra checks for now
+import { Payload } from '../types/payload';
+import { CheckError } from '../types/error';
+import pipe from '../utils/pipe';
 
-export default x => x;
+type keyPayload = Payload<string>;
+
+let notEmpty = (payload: keyPayload) => {
+  if (payload.value.trim().length > 0) {
+    return payload;
+  } else {
+    throw new CheckError(
+      'keys should not be empty!',
+      payload.jessyString,
+      payload.value,
+      'a key string'
+    );
+  }
+};
+
+let noWeirdChars = (payload: keyPayload) => {
+  if (!payload.value.match(/[^\w-]/)) {
+    return payload;
+  } else {
+    throw new CheckError(
+      'keys can only contain a-z A-Z - _ 0-9',
+      payload.jessyString,
+      payload.value.match(/[^\w-]/)[0],
+      'a valid key string'
+    );
+  }
+};
+
+
+export default pipe(notEmpty, noWeirdChars);
